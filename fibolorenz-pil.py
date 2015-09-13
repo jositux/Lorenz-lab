9a298c735072be079bc1e6cb406bd128952b8119
jositux <jm.guaimas@gmail.com>
2015-09-13 19:48:16 -0300

    Update and rename lorenz-pil.py to fibolorenz-pil.py

--- fibolorenz-pil.py
+++ fibolorenz-pil.py
@@ -7,21 +7,22 @@ import random
 from PIL import Image
 imgx = 1024
 imgy = 768
-imagen = Image.new("RGBA", (imgx, imgy))
+imagen = Image.new("RGB", (imgx, imgy))
 
 def fib(n):
  a,b = 1,1
  for i in range(n-1):
   a,b = b,a+b
  return a
-print fib(5)
 
 maxIt = 100000 # Números de pixels a dibujar
-size = 30
+size = 25
 xa = -size
 xb = size
 ya = -size
 yb = size
+
+numFib = 8 # Parámetro de fibonacci
 
 # estados iniciales
 x = random.random() * size * 2 - 1
@@ -31,7 +32,7 @@ z = random.random() * size * 2 - 1
 # dx/dt = delta * (y - x)
 # dy/dt = r * x - y - x * z
 # dz/dt = x * y - b * z
-delta = float(10) # Número de Prandtl https://es.wikipedia.org/wiki/N%C3%BAmero_de_Prandtl
+delta = float(fib(numFib)) # Número de Prandtl, a partir del resultado de fibonacci https://es.wikipedia.org/wiki/N%C3%BAmero_de_Prandtl
 r = float(28)
 b = float(8) / 3
 h = 1e-3 # tiempo del paso
@@ -49,9 +50,9 @@ for i in range(maxIt):
     xi = int((imgx - 1) * (x - xa) / (xb - xa))
     yi = int((imgy - 1) * (y - ya) / (yb - ya))
     if xi >=0 and xi < imgx and yi >= 0 and yi < imgy:
-        imagen.putpixel((xi, yi), (0, 0, 0))
-    
+        imagen.putpixel((xi, yi), (255, 255, 255))
 
-nombre = "Lorenz_%i.gif" % size;
+
+nombre = "Lorenz_%i.png" % size;
 print nombre
-imagen.save( nombre, "GIF", transparency=0)
+imagen.save( nombre, "PNG")
